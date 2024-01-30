bcb9c17530743d5fe22a2d9a16c2bbc54ac6cbe3
Ryguy1127 <91078310+Ryguy1127@users.noreply.github.com>
2024-01-30 07:53:46 -0700

    Update helloteam.js

--- helloteam.js
+++ helloteam.js
@@ -1,6 +1,6 @@
-const teamNames = ['Ryan Larsen', 'Nathan Schmidt', 'Your Name'];
+const teamNames = ['Ryan Larsen', '', 'Your Name'];
 
-const teamNames = ["Ryan Larsen", "Nathan Schmidt"];
+const teamNames = ["Ryan Larsen", ""];
 
 teamNames.forEach(name => {
   console.log(`Hello ${name}`);
